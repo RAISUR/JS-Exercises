18f5879cc7aae6fdb46e129e26ddf1e30bce0658
raisur <raisur.rahman234@gmail.com>
2018-10-04 03:23:43 +0000

    task 1 and task 2

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-/* JavaScript code here */
+console.log("External")
+
+function sumArray(arr){
+    var sum = 0;
+    arr.forEach(function(value,index){
+        sum = sum + value;
+        
+    });
+    return sum;
+    
+}
+console.log(sumArray([2, 4, 5, 6, 7]));
